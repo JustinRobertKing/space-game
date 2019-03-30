67b29492d5458277a9d81d85d14438dfc3edbafd
JustinRobertKing <kingj4@spu.edu>
2019-03-29 18:53:01 -0700

    created parallax background

--- game/js/main.js
+++ game/js/main.js
@@ -2,10 +2,15 @@ console.log('fuck you')
 
 var game = new Phaser.Game(1000, 679, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var space;
+
 function preload() {
 
-    game.load.image('space', 'img/space.jpg');
-    game.load.image('ship1', 'img/ship1.png', 79, 41.5);
+    game.load.image('space2', 'img/space2.jpg');
+    game.load.image('stars1', 'img/stars1.png');
+    game.load.image('stars1Flipped', 'img/stars1Flipped.png');    
+    game.load.image('stars2', 'img/stars2.png');
+    game.load.image('ship1', 'img/ship1.png');
     game.load.image('ship2', 'img/ship2.png');
     game.load.image('ship3', 'img/ship3.png');
     game.load.image('ship4', 'img/ship4.png');
@@ -17,8 +22,11 @@ function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
     //  A simple background for our game
-    game.add.sprite(0, 0, 'space');
-    // The ship and its settings
+    space = game.add.tileSprite(0, 0, 1000, 679, 'space2');
+    // stars2 = game.add.tileSprite(0, 0, 1000, 679, 'stars2')
+    stars1 = game.add.tileSprite(0, 0, 1000, 679, 'stars1')
+    stars1Flipped = game.add.tileSprite(0, 0, 1000, 679, 'stars1Flipped')
+        // The ship and its settings
     var ship = game.add.sprite(0, 0, 'ship1');
     ship.scale.setTo(.5, .5)
 
@@ -28,4 +36,7 @@ function create() {
 }
 
 function update() {
+    stars1.tilePosition.x -= 0.75;
+    stars1Flipped.tilePosition.x -= 0.75;
+    space.tilePosition.x -= 0.6;
 }
